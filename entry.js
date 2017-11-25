0d5e9776dd06b634c83d26a40cb0e7ff7ab21800
Xavier Serrano <zombispormedio007@gmail.com>
2017-11-25 13:38:56 +0100

    add rxjs and redux observable

--- entry.js
+++ entry.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 import ReduxLogger from 'redux-logger'
-import ReduxSaga from 'redux-saga'
+import Rx from 'rxjs'
 import Typography from 'typography'
 import { Scrollbars } from 'react-custom-scrollbars'
 
@@ -10,9 +10,10 @@ window.React = React
 window.ReactDOM = ReactDOM
 window.styled = styled
 window.ReduxLogger = ReduxLogger
-window.ReduxSaga = ReduxSaga
+window.ReduxObservable = require('redux-observable')
 window.Redux = require('redux')
 window.ReactRedux = require('react-redux')
+window.Rx = Rx
 window.R = require('ramda')
 window.Typography = Typography
 window.ReactScrollbars = Scrollbars
